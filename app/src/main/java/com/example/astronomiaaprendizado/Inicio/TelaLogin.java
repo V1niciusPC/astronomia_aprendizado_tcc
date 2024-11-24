4d84fb2a56840b0d537b59424ad8be22a8a7f4a0
vinicius Pereira <viniciuspereirac3@gmail.com>
2024-11-24 02:46:41 -0400

    atualize 03

--- app/src/main/java/com/example/astronomiaaprendizado/Inicio/TelaLogin.java
+++ app/src/main/java/com/example/astronomiaaprendizado/Inicio/TelaLogin.java
@@ -1,5 +1,6 @@
 package com.example.astronomiaaprendizado.Inicio;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -9,18 +10,25 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.astronomiaaprendizado.R;
+import com.example.astronomiaaprendizado.databinding.ActivityTelaLoginBinding;
 
 public class TelaLogin extends AppCompatActivity {
+    private ActivityTelaLoginBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = ActivityTelaLoginBinding.inflate(getLayoutInflater());
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_tela_login);
+        setContentView(binding.getRoot());
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        binding.textCriarConta.setOnClickListener(view -> {
+            startActivity(new Intent(this, TelaCadastro.class));
         });
     }
 }
