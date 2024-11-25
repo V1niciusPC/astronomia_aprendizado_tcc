1c0aff5a542fdab27839e64ec83fbe724e7f6c00
vinicius Pereira <viniciuspereirac3@gmail.com>
2024-11-24 22:33:15 -0400

    atualize 7

--- app/src/main/java/com/example/astronomiaaprendizado/MainActivity.java
+++ app/src/main/java/com/example/astronomiaaprendizado/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.astronomiaaprendizado;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -11,13 +12,16 @@ import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
 
+import com.example.astronomiaaprendizado.Inicio.TelaLogin;
 import com.example.astronomiaaprendizado.databinding.ActivityMainBinding;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
     private NavHostFragment navHostFragment;
     private NavController navController;
+    private FirebaseAuth mAuth;
 
 
     @Override
@@ -31,7 +35,19 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        mAuth = FirebaseAuth.getInstance();
         initNavigation();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentuser = FirebaseAuth.getInstance().getCurrentUser();
+        if(currentuser == null){
+            Intent inten = new Intent(MainActivity.this, TelaLogin.class);
+            startActivity(inten);
+            finish();
+        }
     }
 
     private void initNavigation(){
